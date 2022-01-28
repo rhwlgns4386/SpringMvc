2b49fb58a8117ec72b97bdad2616446beb1a1a6a
rhwlgns4386 <30650239+rhwlgns4386@users.noreply.github.com>
2022-01-28 09:57:12 +0900

    첫 커밋

---
+++ src/main/java/com/hello/demo/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.hello.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+@WebServlet(name = "helloServlet",urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello");
+        System.out.println("username="+req.getParameter("username"));
+
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+        Writer writer=resp.getWriter();
+        writer.write(req.getParameter("username"));
+    }
+}
